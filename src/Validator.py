0644a1e464dfe1dbe971ca4516e9697dc538135d
Erich S <erich.sartison@web.de>
2020-04-07 10:07:49 +0200

    create Validator

--- src/Validator.py
+++ src/Validator.py
@@ -1,27 +1,44 @@
+import sys
+
 DIGITS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 OPERATORS = ["+", "-", "*", "/"]
 PARENTHESIS = ["(", ")"]
-VALID_CHARACTERS = DIGITS + OPERATORS + PARENTHESIS
 IGNORE = [" "]
+VALID_CHARACTERS = DIGITS + OPERATORS + PARENTHESIS
 
 
-
-def isValid(stringexpr):
+# checks if a mathematical expression represented as a String is valid
+def validate(stringexpr) -> bool:
     if stringexpr.count(PARENTHESIS[0]) != stringexpr.count(PARENTHESIS[1]):
         return False
-    
-    expr = [c for c in stringexpr if c not in IGNORE]
 
-    print(expr)
-    """
-    for i in range(len(expr)):
-        if stringexpr in IGNORE:
-            continue
-        if stringexpr[i] not in VALID_CHARACTERS:
+    expr = []
+
+    # ignore spaces except for spacing between digits, in that case return false
+    for i in range(len(stringexpr)):
+        s = stringexpr[i]
+        # check for spaces between digits
+        if len(expr) > 0 and s in DIGITS and expr[-1] in DIGITS and stringexpr[i-1] in IGNORE:
+            print("illegal syntax:\n{}\n{}^".format(stringexpr, " "*(i-1)), file=sys.stderr)
             return False
-        if stringexpr[i] in OPERATORS and len(stringexpr) :
-    """        
+        # check for valid characters
+        if s in VALID_CHARACTERS:
+            expr.append(s)
+        elif s not in IGNORE:
+            print("illegal character: {}\n{}\n{}^".format(s, stringexpr, " "*i, file = sys.stderr))
+            return False
+
+        
+
     
+
+    for i in range(len(expr)):
+        
+        # if expr[i] is an operator or open parenthesis and it's the last character 
+        # or following character isn't a digit or open parenthesis
+        if expr[i] in OPERATORS + [PARENTHESIS[0]] and (len(expr) == i+1 or expr[i+1] not in DIGITS + [PARENTHESIS[0]]) or i == 0 and expr[i] in OPERATORS:   
+            return False
+
     return True
 
-isValid("1 + 2")
+print(validate(" (3) "))
