b87a4d0f9e3e9ea8ca4b8bb3bf94ff7f6e837e5e
f0ggyNights <63108998+f0ggyNights@users.noreply.github.com>
2020-04-07 08:45:50 +0200

    Documentation und __str()__ methode zugefügt

--- src/Mathexpr.py
+++ src/Mathexpr.py
@@ -1,7 +1,18 @@
 class Mathexpr:
+    """Dieser Klasse dient zur Darstellung der Rechnung.
+
+    Das Feld "operator" gibt die durchzuführende Rechenoperation an.
+    Die Felder "left" und "right" enthalten die Beiden Operanden.
+    Die Operanden können entweder eine Zahl oder eine weitere
+    Rechnung sein."""
+
     def __init__(self, operator, left, right):
         self.operator = operator
-        # Operatoren werden als Sring dargestellt: "+", "-", "*", "/"
+        # Operatoren werden als String dargestellt: "+", "-", "*", "/"
 
         self.left = left
         self.right = right
+
+
+    def __str__(self):
+        return ("(" + str(self.operator) + " " + str(self.left) + " " + str(self.right) + ")")
